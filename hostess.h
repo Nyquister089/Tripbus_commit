a18dbae6c499085c081621ce82fdbc42d44695fd
Giordano Di Censo <dicensogiordano@gmail.com>
2022-06-06 18:41:57 +0200

    commit prova

---
+++ hostess.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "../model/trpdb.h"
+
+typedef enum{
+	INFO_PRENOTAZIONI,
+	INSERT_CLIENTE,
+	INSERT_PRENOTAZIONE,
+	INSERT_POSTPRENOTATO,
+	POSTI_VIAGGIO,
+	CONFERMA_PRENOTAZIONE, 
+	UPDATE_DATA_DOC,
+	QUIT
+} hstss_act;
+
+extern void show_prenotation_details(struct prenotazione *prenotazione ); 
+extern void mod_trip_seat (struct viaggio *viaggio); 
+extern void validate_prenotation(struct prenotazione *prenotazione, struct postoprenotato *postoprenotato, struct associata *associata);
+extern void update_d_doc(struct cliente  *cliente); 
