bc8d3a2911f45498c08186a78a71f3571173247a
Giordano Di Censo <dicensogiordano@gmail.com>
2022-06-15 14:00:38 +0200

    Modifica Insert

---
+++ sql/dml/delete_and_insert_utente.sql
@@ -0,0 +1,50 @@
+DELETE FROM `tripdb`.`utente`;
+INSERT INTO `tripdb`.`utente` 
+(`EmailUtente`,
+  `Pswrd`,
+  `TipoUtente`)
+ VALUES 
+ ('1',
+  '1',
+  '3');
+  INSERT INTO `tripdb`.`utente` 
+(`EmailUtente`,
+  `Pswrd`,
+  `TipoUtente`)
+ VALUES 
+ ('drvr@mail.com',
+  '1',
+  '1`);
+  INSERT INTO `tripdb`.`utente` 
+(`EmailUtente`,
+  `Pswrd`,
+  `TipoUtente`)
+ VALUES 
+ ('cstmr@mail.com',
+  '1',
+  '2');
+  INSERT INTO `tripdb`.`utente` 
+(`EmailUtente`,
+  `Pswrd`,
+  `TipoUtente`)
+ VALUES 
+ ('hstss@mail.com',
+  '1',
+  '3');
+  INSERT INTO `tripdb`.`utente` 
+(`EmailUtente`,
+  `Pswrd`,
+  `TipoUtente`)
+ VALUES 
+ ('mch@mail.com',
+  '1',
+  '4');
+  INSERT INTO `tripdb`.`utente` 
+(`EmailUtente`,
+  `Pswrd`,
+  `TipoUtente`)
+ VALUES 
+ ('mngr@mail.com',
+  '1',
+  '5');
+
