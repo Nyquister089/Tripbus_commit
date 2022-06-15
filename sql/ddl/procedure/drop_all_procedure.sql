2c367564ccd8c60d1fae8a8c2a746f91e14de3b3
Giordano Di Censo <dicensogiordano@gmail.com>
2022-06-15 10:48:53 +0200

    aggiunto drop_all_procedure

---
+++ sql/ddl/procedure/drop_all_procedure.sql
@@ -0,0 +1,9 @@
+DROP PROCEDURE IF EXISTS `tripdb`.`insert_assoc`;
+DROP PROCEDURE IF EXISTS `tripdb`.`insert_costumer`;
+DROP PROCEDURE IF EXISTS `tripdb`.`insert_reservation`;
+DROP PROCEDURE IF EXISTS `tripdb`.`insert_seat`;
+DROP PROCEDURE IF EXISTS `tripdb`.`select_costumer`;
+DROP PROCEDURE IF EXISTS `tripdb`.`select_reservation`;
+DROP PROCEDURE IF EXISTS `tripdb`.`select_trip`;
+DROP PROCEDURE IF EXISTS `tripdb`.`update_data_doc`;
+DROP PROCEDURE IF EXISTS `tripdb`.`update_trip_seat`;
---
+++ sql/ddl/view/drop_all_views.sql
@@ -0,0 +1,8 @@
+
+
+DROP VIEW IF EXISTS `tripdb`.`Autista_mete_visitate`;
+DROP VIEW IF EXISTS `tripdb`.`Cliente_mete_tour`;
+DROP VIEW IF EXISTS `tripdb`.`Cliente_servizi_albergo`;
+DROP VIEW IF EXISTS `tripdb`.`Cliente_modello_info`;
+DROP VIEW IF EXISTS `tripdb`.`Hostess_clienti_prenotazioni`;
+DROP VIEW IF EXISTS `tripdb`.`Meccanico_pianifica_revisioni`;
