1a580afcf0eb6af16aad73825f995dbcdc778b4c
Laendor <sim.rus@hotmail.it>
2022-06-25 15:27:12 +0200

    Refactor dati tecnici dei modelli

--- sql/ddl/table/modello.sql
+++ sql/ddl/table/modello.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `tripdb`.`modello` (
   `NomeModello` varchar(45) NOT NULL,
   `Tipologia` varchar(45) NOT NULL,
-  `DatiTecnici` varchar(45) NOT NULL,
+  `DatiTecnici` varchar(250) NOT NULL,
   `CasaCostruttrice` varchar(45) NOT NULL,
   `NumeroPosti` int NOT NULL,
   PRIMARY KEY (`NomeModello`),
--- sql/dml/delete_and_insert_modello.sql
+++ sql/dml/delete_and_insert_modello.sql
@@ -1,7 +1,7 @@
 DELETE FROM `tripdb`.`modello`;
 INSERT INTO `tripdb`.`modello` (`NomeModello`, `Tipologia`, `DatiTecnici`, `CasaCostruttrice`, `NumeroPosti`) 
-VALUES ('AXA', 'UNO', 'Bello', 'ALFA', '10');
+VALUES ('AXA', 'UNO', '	Anche in versione suburbana a 2 porte', 'ALFA ROMEO', '10');
 INSERT INTO `tripdb`.`modello` (`NomeModello`, `Tipologia`, `DatiTecnici`, `CasaCostruttrice`, `NumeroPosti`) 
-VALUES ('DAXA', 'DUE', 'Brutto', 'OMEGA', '12');
+VALUES ('DAXA', 'DUE', 'Carrozzeria in lamiera tradizionale.', 'FIAT', '12');
 INSERT INTO `tripdb`.`modello` (`NomeModello`, `Tipologia`, `DatiTecnici`, `CasaCostruttrice`, `NumeroPosti`) 
-VALUES ('RAXA', 'TRE', 'Insomma', 'ALFA', '14');
+VALUES ('RAXA', 'TRE', 'Anche in versione suburbana a 2 o 3 porte', 'ALFA ROMEO', '14');
