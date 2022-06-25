b172d9b97df376247bc57e8758edc939de7973d3
Laendor <sim.rus@hotmail.it>
2022-06-25 19:03:41 +0200

    Correzione drop_all_view

--- sql/ddl/view/drop_all_views.sql
+++ sql/ddl/view/drop_all_views.sql
@@ -6,3 +6,4 @@ DROP VIEW IF EXISTS `tripdb`.`Cliente_servizi_albergo`;
 DROP VIEW IF EXISTS `tripdb`.`Cliente_modello_info`;
 DROP VIEW IF EXISTS `tripdb`.`Hostess_cliente_prenotazioni`;
 DROP VIEW IF EXISTS `tripdb`.`Meccanico_pianifica_revisione`;
+DROP VIEW IF EXISTS `tripdb`.`Mezzi_Con_Revisione_Scaduta`;
